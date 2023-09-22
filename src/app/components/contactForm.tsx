111cd0cf5054441222dcdda4fa8b5eae8dc53c1e
Sylvain <sylvain.maurier@gmail.com>
2023-09-22 19:00:44 +0200

    implementation form

--- src/app/components/contactForm.tsx
+++ src/app/components/contactForm.tsx
@@ -1,61 +1,13 @@
-"use client"
+'use client'
 
-import { useState } from "react";
+import { Widget } from '@typeform/embed-react'
 
-function ContactForm() {
-  const [formData, setFormData] = useState({
-    name: '',
-    subject: '',
-    message: '',
-  });
-
-  const handleChange(event) {
-    const { name, value } = event.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
+const ContactForm = () => {
   return (
-    <form method="POST" data-netlify="true">
-      <div>
-        <label htmlFor="name">Nom :</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="subject">Objet :</label>
-        <input
-          type="text"
-          id="subject"
-          name="subject"
-          value={formData.subject}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="message">Texte à envoyer :</label>
-        <textarea
-          id="message"
-          name="message"
-          value={formData.message}
-          onChange={handleChange}
-          required
-        ></textarea>
-      </div>
-      <div>
-        <button type="submit">Envoyer</button>
-      </div>
-    </form>
+    <>
+        <Widget id="mSdsYqjT" style={{ width: '100%', height: 'calc(100vh - 50px)' }} className="my-form" />
+    </>
   );
-}
+};
 
 export default ContactForm;
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,8 +5,6 @@ export default function Contact() {
   return (
     <>
       <Header />
-      <h2>Contactez-moi</h2>
-      <p>Utilisez le formulaire ci-dessous pour nous envoyer un message :</p>
       <ContactForm />
     </>
   );
